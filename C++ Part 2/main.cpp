9f54d21b4061e595512201f5484f76bc59e4b056
Rafael V <rafaelv.@Rafaels-MacBook-Pro.local>
2017-10-03 23:35:41 +0100

    Initial Commit

---
+++ C++ Part 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Part 2
+//
+//  Created by Rafael V. on 10/3/17.
+//  Copyright © 2017 Rafael V. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
